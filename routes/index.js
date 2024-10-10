13012412a68e6859bbd0c475e98ef2efe995ecd7
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-10 11:48:54 -0400

    Add Swagger documentation route and fix newline issue

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
+router.use('/', require('./swagger'));
+
 router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 router.use('/users', require('./users'));
+
 
 module.exports = router;
