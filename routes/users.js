a2b9c901476b1d324a17cef0c6aba1309e2ddc5d
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-10-10 11:49:02 -0400

    Refactor users controller and add CRUD operations

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAll, getSingle } = require('../controllers/users');
+const { getAll, getSingle, createUser, updateUser, deleteUser } = require('../controllers/users');
 
 router.get('/', getAll);
 router.get('/:id', getSingle);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
